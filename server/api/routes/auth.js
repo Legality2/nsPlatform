dcd77cb926db27b9b927dfc07da5824c30d30247
Paul newman <pnewman@maxamtech.com>
2019-12-18 00:27:49 -0500

    file updates

--- server/api/routes/auth.js
+++ server/api/routes/auth.js
@@ -20,10 +20,10 @@ function verifyToken(req, res, next) {
       console.log.decoded;
       next();
     });
-  }
+  };
 
 //get user logged in info 
-router.get('/me', VerifyToken, function(req, res, next) {
+router.get('/me', verifyToken, function(req, res, next) {
 
     User.findById(req.userId, { password: 0 }, function (err, user) {
       if (err) return res.status(500).send("There was a problem finding the user.");
